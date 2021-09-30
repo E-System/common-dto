63e0ed73aef86f265d6877601a49e5cc4beee396
diman <zuzoev.d@ext-system.com>
2021-09-30 15:20:01 +0700

    Add oauth2 DTO

---
+++ src/main/java/com/es/lib/dto/oauth2/DTOToken.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) E-System LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by E-System team (https://ext-system.com), 2018
+ */
+
+package com.es.lib.dto.oauth2;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * @author Dmitriy Zuzoev - zuzoev.d@ext-system.com
+ * @since 09.02.2018
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class DTOToken implements Serializable {
+
+    @JsonProperty(value = "access_token")
+    private String accessToken;
+    @JsonProperty(value = "token_type")
+    private String tokenType;
+    @JsonProperty(value = "refresh_token")
+    private String refreshToken;
+    @JsonProperty(value = "expires_in")
+    private long expiresIn;
+    @JsonProperty(value = "scope")
+    private String scope;
+    @JsonProperty(value = "jti")
+    private String jti;
+}
